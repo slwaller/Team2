50448e8698ffbcf7a1bed832b57c8cfce4dfe40f
Sam Waller <Sam@Sams-MacBook-Air.local>
2014-09-17 09:33:11 -0400

    bootstrap and email field to student

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140916213205) do
+ActiveRecord::Schema.define(version: 20140917133206) do
+
+  create_table "students", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "bio"
+    t.string   "github"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "email"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
