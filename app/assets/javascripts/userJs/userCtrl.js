ef1c483eecccf6de4b1130307432adbde175f677
Ansley Jones <ansleyjones@Ansleys-MacBook-Pro.local>
2014-09-18 15:14:59 -0400

    fixed form

--- app/assets/javascripts/userJs/userCtrl.js
+++ app/assets/javascripts/userJs/userCtrl.js
@@ -1,20 +1,29 @@
 angular.module("cdpApp")
-  .controller('userCtrl', function ($route, $rootScope, $routeParams, $scope, $log, userSvc) {
-    userSvc.getUsers().success(function(Users){
-      $scope.Users = Users;
+  .controller('userCtrl', function ($route, $rootScope, $routeParams, $scope, $log, $location, userSvc) {
 
+    userSvc.getUsers().success(function(Users){
+      $scope.Users = Users.users;
+      $scope.currentUser = Users.user;
+      console.log($scope.Users);
     });
 
-    userSvc.getUserProfile($routeParams.idx).success(function(User){
+    userSvc.getUserProfile($routeParams.id).success(function(User){
       $scope.User = User;
       console.log(User);
 
     });
 
+    // $scope.show = function(id){
+    //   userSvc.getUserProfile(id).success(function(User){
+    //     $scope.User = User;
+    //     $location.path("/user/id");
+    // }
     var createUser = function (User){
-
       userSvc.createUser(User);
     };
 
+    var editUser = function(id){
+      userSvc.editUserProfile(id);
+    }
 
   });
