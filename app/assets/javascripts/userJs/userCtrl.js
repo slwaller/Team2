336b7581338a2ca4466eeeb0065bb329dfb239d6
Ansley Jones <ansleyjones@Ansleys-MacBook-Pro.local>
2014-09-18 16:24:41 -0400

    filter added

--- app/assets/javascripts/userJs/userCtrl.js
+++ app/assets/javascripts/userJs/userCtrl.js
@@ -2,8 +2,8 @@ angular.module("cdpApp")
   .controller('userCtrl', function ($route, $rootScope, $routeParams, $scope, $log, $location, userSvc) {
 
     userSvc.getUsers().success(function(Users){
-      $scope.Users = Users.users;
-      $scope.currentUser = Users.user;
+      $rootScope.Users = Users.users;
+      $rootScope.currentUser = Users.user;
       console.log($scope.Users);
     });
 
@@ -13,17 +13,26 @@ angular.module("cdpApp")
 
     });
 
-    // $scope.show = function(id){
-    //   userSvc.getUserProfile(id).success(function(User){
-    //     $scope.User = User;
-    //     $location.path("/user/id");
-    // }
     var createUser = function (User){
       userSvc.createUser(User);
     };
 
     var editUser = function(id){
       userSvc.editUserProfile(id);
-    }
+    };
 
+  })
+
+  .filter('matchFilter', function() {
+    return function (userObj, curUser) {
+      var filtered_list = [];
+      console.log(userObj);
+      console.log(curUser);
+      for (var i = 0; i < userObj.length; i++) {
+        if (curUser.weakness == userObj[i].strengths) {
+          filtered_list.push(userObj[i]);
+        }
+      }
+      return filtered_list;
+    }
   });
