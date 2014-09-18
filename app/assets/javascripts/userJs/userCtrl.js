8eb5d85d0c72d45f9edfa330d3df2085603d821b
Ansley Jones <ansleyjones@Ansleys-MacBook-Pro.local>
2014-09-18 09:04:10 -0400

    styling

--- app/assets/javascripts/userJs/userCtrl.js
+++ app/assets/javascripts/userJs/userCtrl.js
@@ -1,11 +1,11 @@
 angular.module("cdpApp")
-  .controller('userCtrl', function ($route, $rootScope, $scope, $log, userSvc) {
+  .controller('userCtrl', function ($route, $rootScope, $routeParams, $scope, $log, userSvc) {
     userSvc.getStudents().success(function(students){
       $scope.students = students;
 
     });
 
-    userSvc.getStudentProfile().success(function(student){
+    userSvc.getStudentProfile($routeParams.idx).success(function(student){
       $scope.student = student;
       console.log(student);
 
