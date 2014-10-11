40a3bff53246cb1ef736c6cd54991a34b6a0ddb4
slwaller <hockeywalt@yahoo.com>
2014-10-11 17:18:58 -0400

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140917224245) do
-
-  create_table "students", force: true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "bio"
-    t.string   "github"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "email"
-    t.string   "workflow_state"
-    t.string   "technique"
-  end
+ActiveRecord::Schema.define(version: 20140918134950) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
